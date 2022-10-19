d3c87404153578d5c3ba35319550b957eebc929a
rundnc <45864055+rundnc@users.noreply.github.com>
2022-10-19 09:38:21 -0700

    Update pyqt_testing.py

--- pyqt_testing.py
+++ pyqt_testing.py
@@ -44,33 +44,7 @@ def get_avg(hum_list, temp_list):
 
 hum_avg, temp_avg, cur_time = get_avg(hum_list, temp_list)
 
-class WindowExample(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setGeometry(200, 200, 400, 300)
-        self.setWindowTitle("My Second Window!")
-        self.setWindowIcon(QIcon('houdini_logo.png'))
-
-        self.setStyleSheet('background-color: blue')
-        
-        self.create_buttons()
-
-    def create_buttons(self):
-
-        button1 = QPushButton("Yes", self)
-        button1.setGeometry(100,100, 100,100)
-        #button1.setIcon(QIcon('houdini_logo.png'))
-        button1.setStyleSheet('color: red; background-color: black')
-
-        button2 = QPushButton("No", self)
-        button2.setGeometry(200,100, 100,100)
-        #button2.setIcon(QIcon('houdini_logo.png'))
-        button2.setStyleSheet('color: orange; background-color: black')
-
-def export_csv(n, hum_avg, temp_avg, cur_time, self):
-
-    #radio_button = self.sender()
+def export_csv(n, hum_avg, temp_avg, cur_time):
 
     final_data = [hum_avg, temp_avg, cur_time]
     file_name = "Jar" + n + ".csv"
@@ -83,32 +57,29 @@ def export_csv(n, hum_avg, temp_avg, cur_time, self):
         print("File write complete")
 
     else:
+        def create_csv():
 
-        #create_file = input("Selected file does not currently exist. Would you like to create new file Jar" + n + ".csv?: Y or N ")
-        app = QApplication(sys.argv)
-        window = WindowExample()
-        window.show()
-        #sys.exit(app.exec_())
-        
-        
-
-        if radio_button.isChecked():
-            self.label.setText(f'You have selected: {radio_button.text()}')
-        #if create_file == "Y" or create_file == "y":
-
+            #self.label.setText(f'You have created a new CSV file. ')
             with open(file_name, 'w') as csvfile:
                 new_file = csv.writer(csvfile)
                 new_file.writerow(['Humidity ', ' Temp ', ' Date and Time'])
                 new_file.writerow(final_data)
-            print(file_name + " has been created and is ready for use.")
+                print(file_name + " has been created and is ready for use.")
 
-        else:
-            print("No file was created. Closing the program.")
+        app = QApplication(sys.argv)
+        widget = QWidget()
 
+        button1 = QPushButton(widget)
+        button1.setText('Yes')
+        button1.move(64, 32)
+        button1.clicked.connect(create_csv)
+
+        widget.setGeometry(50,50,720,200)
+        widget.setWindowTitle("Selected file does not currently exist. Would you like to create new file Jar" + n + ".csv?")
+        widget.show()
+        sys.exit(app.exec_())
+
+        #create_file = input("Selected file does not currently exist. Would you like to create new file Jar" + n + ".csv?: Y or N ")
 
 export_csv(n, hum_avg, temp_avg, cur_time)
 
-current_dir = str(pathlib.Path(__file__).parent.absolute())
-path = current_dir + 'houdini_logo.png'
-
-#if __name__ == '__main__':
