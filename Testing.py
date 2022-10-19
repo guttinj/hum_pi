4a6f2f3a79104c4b2e43cdc44d0487edaf00c0ad
Dakota-Guttin <89602347+Dakota-Guttin@users.noreply.github.com>
2022-10-18 23:25:42 -0600

    Create Testing.py

---
+++ Testing.py
@@ -0,0 +1,80 @@
+#import Adafruit_DHT
+import datetime
+import csv
+# import threading
+import time
+import os
+
+# cur_time = datetime.datetime.now()
+# str_cur_time = str(cur_time)
+# sen = Adafruit_DHT.DHT11
+# pin_Num = 4
+humidity = 0
+temp = 0
+date = 0
+
+n = input("Which Jar would you like to measure : ")
+
+def sense_forever(humidity, temp, date):#get humidity and temperature values
+    count = 0
+    # lst = []
+    hum_list = []
+    temp_list = []
+    while True:
+        if count <= 4:
+
+            date = str(datetime.datetime.now())
+
+            ##humidity, temp = Adafruit_DHT.read_retry(sen, pin_Num)
+            
+            humidity += 5
+            temp += 12
+
+            ##print('reading sensor ' , date) 
+            time.sleep(1)
+            #tempor = (("Temperature: "+ str(temp)), ("Humidity: "+ str(humidity)), ("Date and Time: " + date))
+            #lst.append(tempor)
+            hum_list.append(humidity)
+            temp_list.append(temp)
+            count = count + 1
+            #print(hum_list, temp_list)   
+        else:
+            break
+    return(hum_list, temp_list)
+        # print(lst, count, len(lst))
+        #make data accessible for later function1
+
+hum_list, temp_list = sense_forever(humidity, temp, date)
+
+def get_avg(hum_list, temp_list):
+    hum_avg = sum(hum_list) / len(hum_list)
+    temp_avg = sum(temp_list) / len(temp_list)
+    date = str(datetime.datetime.now())
+    return(hum_avg, temp_avg, date)
+
+hum_avg, temp_avg, cur_time = get_avg(hum_list, temp_list)
+
+def export_csv(n, hum_avg, temp_avg, cur_time):
+    final_data = [hum_avg, temp_avg, cur_time]
+    file_name = "Jar" + n + ".csv"
+    #chosen_file = open(file_name, 'a')
+
+    if os.path.exists(file_name):
+        chosen_file = open(file_name, 'a')
+        open_file = csv.writer(chosen_file)
+        open_file.writerow(final_data)
+        print("File write complete")
+    else:
+        create_file = input("Selected file does not currently exist. Would you like to create new file Jar" + n + ".csv?: Y or N")
+        
+        if create_file == "Y" or create_file == "y":
+            with open(file_name, 'w') as csvfile:
+                new_file = csv.writer(csvfile)
+                new_file.writerow(['Humidity ', ' Temp ', ' Date and Time'])
+            print("New file has been created. Please rerun the program using the new file")
+        else:
+            print("No file was created. Closing the program.")
+
+
+export_csv(n, hum_avg, temp_avg, cur_time)
+
